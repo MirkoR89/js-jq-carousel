2137f36edf4e896c20e192e6d78f4f62ed62a6c5
Mirko <mirko.rinelli@gmail.com>
2020-11-29 23:07:32 +0100

    Prev img

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,24 @@
-$(".next").click(function() {
-  if ($(".last").hasClass("active")) {
+$(document).ready(function() {
+
+  // Funzione per scorrere le immagini in avanti
+  $(".next").click(function() {
+    if ($(".last").hasClass("active")) {
       $(".first").addClass("active");
       $(".last").removeClass("active");
     } else {
       $(".active").next().addClass("active");
       $(".active").prev().removeClass("active");
     }
+  });
+
+  // Funzione per scorrere le immagini indietro
+  $(".prev").click(function () {
+    if ($(".first").hasClass("active")) {
+      $(".last").addClass("active");
+      $(".first").removeClass("active");
+    } else {
+      $(".active").prev().addClass("active");
+      $(".active").next().removeClass("active");
+    }
+  })
 });
